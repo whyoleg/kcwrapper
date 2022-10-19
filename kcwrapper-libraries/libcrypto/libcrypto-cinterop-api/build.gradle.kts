736e02fcba2405520481b01518fac1ef07805347
Oleg Yukhnevich <whyoleg@gmail.com>
2022-10-19 16:05:59 +0300

    experiment with splitting def files

--- kcwrapper-libraries/libcrypto/libcrypto-cinterop-api/build.gradle.kts
+++ kcwrapper-libraries/libcrypto/libcrypto-cinterop-api/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.*
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
@@ -8,9 +10,23 @@ kotlin {
 //    macosArm64()
     mingwX64 {
         val main by compilations.getting {
-            val cinterop by cinterops.creating {
-                defFile("cinterop.def")
+            val bn by cinterops.creating {
+                defFile("bn.def")
             }
+            val bnCP = tasks.getByName<CInteropProcess>(bn.interopProcessingTaskName)
+            val bio by cinterops.creating {
+                defFile("bio.def")
+                extraOpts("-library", bnCP.outputFileProvider.get())
+            }
+            tasks.named(bio.interopProcessingTaskName).get().dependsOn(bnCP)
+            val bioCP = tasks.getByName<CInteropProcess>(bio.interopProcessingTaskName)
+            val evp by cinterops.creating {
+                defFile("evp.def")
+                extraOpts("-library", bioCP.outputFileProvider.get())
+                extraOpts("-library", bnCP.outputFileProvider.get())
+            }
+            tasks.named(evp.interopProcessingTaskName).get().dependsOn(bioCP)
+            tasks.named(evp.interopProcessingTaskName).get().dependsOn(bnCP)
         }
     }
     sourceSets {
--- kcwrapper-libraries/libcrypto/libcrypto-cinterop-tests/sources/test.kt
+++ kcwrapper-libraries/libcrypto/libcrypto-cinterop-tests/sources/test.kt
@@ -1,4 +1,7 @@
 import kcwrapper.libcrypto.interop.*
+import kcwrapper.libcrypto.interop.bn.*
+import kcwrapper.libcrypto.interop.bio.*
+import kcwrapper.libcrypto.interop.evp.*
 import kotlinx.cinterop.*
 import kotlin.test.*
 
@@ -12,7 +15,7 @@ class SimpleTest {
 //        println(result)
         val data = "Hello World".encodeToByteArray().asUByteArray()
         val hash = ByteArray(32).asUByteArray()
-        val result = SHA256(data.refTo(0), data.size.convert(), hash.refTo(0))
-        println(hash.asByteArray().decodeToString())
+//        val result = SHA256(data.refTo(0), data.size.convert(), hash.refTo(0))
+//        println(hash.asByteArray().decodeToString())
     }
 }
