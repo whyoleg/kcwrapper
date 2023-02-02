f17f6a9d48b682b5cdda90678a8de0d7fabb2ed3
Oleg Yukhnevich <whyoleg@gmail.com>
2023-02-02 14:28:28 +0300

    try[1]

--- libraries/libcrypto3/dynamic/build.gradle.kts
+++ libraries/libcrypto3/dynamic/build.gradle.kts
@@ -1,13 +1,11 @@
-import dev.whyoleg.kcwrapper.gradle.plugin.*
 import org.jetbrains.kotlin.gradle.plugin.mpp.*
-import org.jetbrains.kotlin.gradle.targets.native.tasks.*
-import org.jetbrains.kotlin.gradle.tasks.*
-import org.jetbrains.kotlin.konan.target.*
 
 plugins {
     id("buildx-multiplatform-library")
     id("dev.whyoleg.kcwrapper")
 }
+
+val prebuiltSetup = kcwrapper.root.openssl3PrebuiltSetupTaskProvider
 
 kotlin {
     nativeDesktopTargets()
@@ -29,28 +27,26 @@ kotlin {
         if (this !is KotlinNativeTarget) return@all
 
         val main by compilations.getting {
-            val api by cinterops.creating {
+            val linking by cinterops.creating {
                 defFile("linking.def")
             }
         }
     }
 }
 
-val prebuiltSetup = kcwrapper.root.openssl3PrebuiltSetupTaskProvider
+//tasks.withType<KotlinNativeLink>().configureEach {
+//    dependsOn(prebuiltSetup)
+//    binary.linkerOpts("-L${prebuiltSetup.get().libDir(binary.target.konanTarget.toOpenssl3Target()).absolutePath}")
+//}
 
-tasks.withType<KotlinNativeLink>().configureEach {
-    dependsOn(prebuiltSetup)
-    binary.linkerOpts("-L${prebuiltSetup.get().libDir(binary.target.konanTarget.toOpenssl3Target()).absolutePath}")
-}
-
-tasks.withType<KotlinNativeTest>().configureEach {
-    dependsOn(prebuiltSetup)
-
-    val libraryPath = prebuiltSetup.get().libDir(HostManager.host.toOpenssl3Target()).absolutePath
-    when (HostManager.host) {
-        KonanTarget.LINUX_X64                          -> environment("LD_LIBRARY_PATH", libraryPath)
-        KonanTarget.MACOS_X64, KonanTarget.MACOS_ARM64 -> environment("DYLD_LIBRARY_PATH", libraryPath)
-        KonanTarget.MINGW_X64                          -> environment("PATH", libraryPath)
-        else                                           -> error("Unsupported host: ${HostManager.host}")
-    }
-}
+//tasks.withType<KotlinNativeTest>().configureEach {
+//    dependsOn(prebuiltSetup)
+//
+//    val libraryPath = prebuiltSetup.get().libDir(HostManager.host.toOpenssl3Target()).absolutePath
+//    when (HostManager.host) {
+//        KonanTarget.LINUX_X64                          -> environment("LD_LIBRARY_PATH", libraryPath)
+//        KonanTarget.MACOS_X64, KonanTarget.MACOS_ARM64 -> environment("DYLD_LIBRARY_PATH", libraryPath)
+//        KonanTarget.MINGW_X64                          -> environment("PATH", libraryPath)
+//        else                                           -> error("Unsupported host: ${HostManager.host}")
+//    }
+//}
